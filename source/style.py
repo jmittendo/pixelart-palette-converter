b9b4aa5a9c57bf0b65f1816c7f6b0f595f44e338
Jan Mittendorf <jan.mittendorf@freenet.de>
2023-12-17 12:13:10 +0100

    Add license text and reference to style.py

--- source/style.py
+++ source/style.py
@@ -1,3 +1,19 @@
+# This file is part of pixelart-palette-converter.
+# Copyright (C) 2023  Jan Mittendorf
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import typing
 
 from PyQt6.QtCore import QRect, Qt
@@ -85,6 +101,9 @@ class AppStyle(QCommonStyle):
     ) -> None:
         match control:
             case QStyle.ComplexControl.CC_GroupBox:
+                # Implementation adapted from:
+                # https://github.com/qt/qtbase/blob/dev/src/widgets/styles/qcommonstyle.cpp
+
                 group_box_option = typing.cast(QStyleOptionGroupBox, option)
 
                 # Draw frame
